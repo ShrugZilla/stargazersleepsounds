189818f55264491be6e83b6def4a429ac1354e89
ShrugZilla <tylerlovekeeper@gmail.com>
2025-06-09 19:38:48 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,25 @@
-self.addEventListener('install', function(e) {
-  e.waitUntil(
-    caches.open('sleep-sounds').then(function(cache) {
-      return cache.addAll([
-        './',
-        './index.html',
-        './manifest.json'
-      ]);
-    })
+const CACHE_NAME = "stargazer-cache-v1";
+const urlsToCache = [
+  "/stargazersleepsounds/",
+  "/stargazersleepsounds/index.html",
+  "/stargazersleepsounds/manifest.json",
+  "/stargazersleepsounds/icon-192.png",
+  "/stargazersleepsounds/icon-512.png",
+  "/stargazersleepsounds/roof-rainfall-ambient-drizzle-in-nature-335712.mp3",
+  "/stargazersleepsounds/thunderstorm-ambiance-191991.mp3",
+  "/stargazersleepsounds/wind.mp3"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener('fetch', function(e) {
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
-    })
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
   );
 });
